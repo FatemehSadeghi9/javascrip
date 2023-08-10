2fbd85855bd9b2ec74cb8fd8d837f94b22ab3b9a
Fatemeh <fatemeh.sadeghi.personal@gmail.com>
2023-08-10 10:27:45 +0100

    frist train of javascript

---
+++ node.js
@@ -0,0 +1,22 @@
+var a=2;
+var b='khar';
+var c;
+var d=null;
+var n=true;
+console.log(n);
+console.log(typeof n)
+var k={
+    FristName: "Fatemeh",
+    SecondName: "Sadeghi"
+}
+console.log(k.FristName);
+console.log(k["FristName"]);
+var l={
+    l1:{
+        l2:{
+         FristName: "Fatemeh",
+         SecondName: "Sadeghi"   
+        }
+    }
+}
+console.log(l.l1.l2.SecondName);
